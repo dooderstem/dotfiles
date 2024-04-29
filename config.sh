e8d25fc39115d7dc40df36179ebb2ec25bd9d676
dooderstem <skyviewsmith@gmail.com>
2024-04-29 16:37:12 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,58 +1,38 @@
-# This file is a work in progress.
+#! bin/bash
 
-DOTFILES=(.bash_profile .bashrc .bash_aliases .bash_env.sh .gitconfig)
-
-choco_install() {
-    # manage corsair preferables
-    choco install icue
-
-    # Browsers
-    choco install googlechrome
-    choco install opera-gx
-
-    # Auth
-    choco install bitwarden
-    choco install bitwarden-cli
-
-    # Productivity
-    choco install notion
-    choco install todoist
-    choco install obsidian
-    choco install chatgpt
-    choco install sparkmail
-
-    # DEV
-    choco install vscode
-    choco install micro
-    choco install nodejs
-    choco install nvm
-    choco install python
-    choco install ruby
-    choco install git
-    choco install gh
-    choco install github-desktop
-
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-
-    ## Design
-    choco install gimp
-    choco install inkscape
-    choco install obs-studio
-
-    ## Other
-    choco install steam-client
-    choco install discord
-
+update(){
+	sudo apt update
+	sudo apt upgrade
 }
 
-install_vcprompt() {
-    curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt >~/bin/vcprompt
-    chmod 755 ~/bin/vcprompt
+installs(){
+	sudo apt install code
+	sudo apt install git
+	sudo apt install gh	
+	sudo apt install flatpak
+	sudo apt install nodejs
+	
+	flatpak install flathub\
+ 	com.raggesilver.BlackBox\
+ 	io.dbeaver.DBeaverCommunity\
+ 	io.github.zyedidia.micro\
+ 	com.opera.Opera\
+ 	com.microsoft.Edge\
+ 	com.bitwarden.desktop\
+ 	xyz.woxel.Woxel\
+ 	net.minetest.Minetest\
+ 	com.obsproject.Studio\
+ 	md.obsidian.Obsidian\
+ 	org.gimp.GIMP\
 }
 
-# git config --global core.editor micro
-# git config --global user.name "dooderstem"
-# git config --global user.email "johnDoe@wright.edu"
+git_conf(){
+	git config --global core.editor micro
+	git config --global user.name $1
+	git config --global user.email $2
+}
 
-# Installing Chocolatey this way requires Powershell:
-    # Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+mkdir ~/bin
+curl -sL https://github.com/djl/vcprompt/raw/master/bin/vcprompt > ~/bin/vcprompt
+chmod 755 ~/bin/vcprompt
